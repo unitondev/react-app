9835f1c4eea21d3e81f973139b4e8c78ab966691
Artyom Korbut <lovrovalx@gmail.com>
2021-06-12 23:40:30 +0300

    added validation by using formik and yup

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -4,38 +4,10 @@ import LoginView from "../views/Login/LoginView";
 class LoginContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            email: "",
-            password: "",
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
-
-    handleSubmit(event) {
-        console.log(
-            `email: ${this.state.email}, password ${this.state.password}`
-        );
-        event.preventDefault();
-    }
-
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value,
-        });
-    }
-
-    handleValidation() {}
 
     render() {
-        return (
-            <LoginView
-                handleSubmit={this.handleSubmit}
-                handleChange={this.handleChange}
-                email={this.state.email}
-                password={this.state.password}
-            />
-        );
+        return <LoginView />;
     }
 }
 
--- src/views/Login/LoginView.js
+++ src/views/Login/LoginView.js
@@ -4,36 +4,58 @@ import NavBarView from "./../../views/Navbar/NavBarView";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-function LoginView({ handleSubmit, handleChange, email, password, classes }) {
+function LoginView({ classes }) {
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().email("Invalid email").required("Required"),
+            password: Yup.string()
+                .min(6, "Must be more than 6 characters ")
+                .required("Required"),
+        }),
+        onSubmit: (values) => {
+            console.log(JSON.stringify(values));
+        },
+    });
     return (
         <>
             <NavBarView />
             <div>
-                <form onSubmit={handleSubmit} className={classes.loginBlock}>
+                <form
+                    onSubmit={formik.handleSubmit}
+                    className={classes.loginBlock}
+                >
                     <TextField
-                        required
                         className={classes.textField}
                         type="text"
                         name="email"
-                        value={email}
-                        onChange={handleChange}
+                        {...formik.getFieldProps("email")}
                         id="outlined-basic"
                         label="Enter Email"
                         variant="outlined"
                     ></TextField>
+                    {formik.touched.email && formik.errors.email ? (
+                        <p>{formik.errors.email}</p>
+                    ) : null}
 
                     <TextField
-                        required
                         className={classes.textField}
                         type="password"
                         name="password"
-                        value={password}
-                        onChange={handleChange}
+                        {...formik.getFieldProps("password")}
                         id="outlined-basic"
                         label="Enter Password"
                         variant="outlined"
                     ></TextField>
+                    {formik.touched.password && formik.errors.password ? (
+                        <p>{formik.errors.password}</p>
+                    ) : null}
 
                     <Button variant="contained" color="secondary" type="submit">
                         Log In
@@ -42,8 +64,8 @@ function LoginView({ handleSubmit, handleChange, email, password, classes }) {
             </div>
 
             <div className={classes.loginBlock}>
-                <p>email: {email}</p>
-                <p>password: {password}</p>
+                <p>email: {JSON.stringify(formik.values.email)}</p>
+                <p>password: {JSON.stringify(formik.values.password)}</p>
             </div>
         </>
     );
@@ -51,10 +73,6 @@ function LoginView({ handleSubmit, handleChange, email, password, classes }) {
 
 LoginView.propTypes = {
     classes: PropTypes.object.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    handleChange: PropTypes.func.isRequired,
-    email: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired,
 };
 
 export default withStyles(styles)(LoginView);
